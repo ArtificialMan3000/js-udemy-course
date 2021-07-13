4be9ed37dcb54a77c43c8548abf8dafdcd5eabda
Aleksandr <legionerab3000@yandex.ru>
2021-07-13 16:56:19 +0300

    Practice 7 | Modal

--- practice_7/src/js/main.js
+++ practice_7/src/js/main.js
@@ -111,4 +111,74 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     setClock('.timer', deadline);
+
+    // Модальное окно
+
+    // Элемент модального окна
+    const modalElem = document.querySelector('.modal');
+    // Элемент диалоговой части модального окна
+    const modalDialogElem = document.querySelector('.modal__dialog');
+    // Кнопки закрытия модального окна
+    const modalCloseElems = document.querySelectorAll('[data-close]');
+    // Кнопки вызова модального окна
+    const modalCallElems = document.querySelectorAll('[data-modal');
+
+    // Открывает модальное окно
+    const openModal = () => {
+        if (modalElem) {
+            modalElem.classList.add('modal_opened');
+            // Запрещаем странице прокручиваться по вертикали
+            document.body.style.overflowY = 'hidden';
+        }
+    };
+
+    // Закрывает модальное окно
+    const closeModal = () => {
+        if (modalElem) {
+            modalElem.classList.remove('modal_opened');
+            // Разрешаем странице прокручиваться по вертикали
+            document.body.style.overflowY = '';
+        }
+    };
+
+    // Устанавливаем обработчики на кнопки вызова модального окна
+    if (modalCallElems) {
+        modalCallElems.forEach((modalCallElem) => {
+            modalCallElem.addEventListener('click', (evt) => {
+                evt.preventDefault();
+                // Открываем окно
+                openModal();
+            });
+        });
+    }
+
+    // Устанавливаем обработчики на кнопки закрытия модального окна
+    if (modalCloseElems) {
+        modalCloseElems.forEach((modalCloseElem) => {
+            modalCloseElem.addEventListener('click', (evt) => {
+                evt.preventDefault();
+                // Закрываем окно
+                closeModal();
+            });
+        });
+    }
+
+    // Устанавливаем обработчик на клик вне модального окна
+    if (modalElem && modalDialogElem) {
+        modalElem.addEventListener('click', (evt) => {
+            // Закрываем окно, если клик был вне диалоговой части
+            if (!evt.target.closest('.modal__dialog')) {
+                closeModal();
+            }
+        });
+    }
+
+    // Устанавливаем обработчик на нажатие ESC
+    if (modalElem) {
+        document.addEventListener('keydown', (evt) => {
+            if (evt.code === 'Escape' && modalElem.classList.contains('modal_opened')) {
+                closeModal();
+            }
+        });
+    }
 });

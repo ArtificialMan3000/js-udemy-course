ac60d59871fd8ae34264855c57f95c5ca59770d5
Aleksandr <legionerab3000@yandex.ru>
2021-07-14 21:20:27 +0300

    Practice 7 | Cards on classes

--- practice_7/dist/js/script.js
+++ practice_7/dist/js/script.js
@@ -211,8 +211,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (modalElem) {
       modalElem.classList.add('modal_opened'); // Запрещаем странице прокручиваться по вертикали
 
-      document.body.style.overflowY = 'hidden';
-      clearInterval(modalTimerId);
+      document.body.style.overflowY = 'hidden'; // clearInterval(modalTimerId);
     }
   }; // Закрывает модальное окно
 
@@ -264,9 +263,9 @@ document.addEventListener('DOMContentLoaded', () => {
         closeModal();
       }
     });
-  }
+  } // const modalTimerId = setTimeout(openModal, 5000);
+  // Обработчик прокрутки страницы
 
-  const modalTimerId = setTimeout(openModal, 5000); // Обработчик прокрутки страницы
 
   const windowScrollHandler = () => {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -276,7 +275,114 @@ document.addEventListener('DOMContentLoaded', () => {
   }; // Устанавливаем обработчик на прокрутку страницы
 
 
-  window.addEventListener('scroll', windowScrollHandler);
+  window.addEventListener('scroll', windowScrollHandler); // Используем классы для создания карточек меню
+
+  class MenuCard {
+    constructor(imgSrc, imgAlt, title, description, price) {
+      this.imgSrc = imgSrc;
+      this.imgAlt = imgAlt;
+      this.title = title;
+      this.description = description;
+      this.price = price; // Курс гривны к доллару
+
+      this.transfer = 27; // Конвертируем
+
+      this.changeToUAH();
+    } // Конвертирует доллары в гривны
+
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    } // Создаёт пустую карточку меню
+
+
+    createCardElem() {
+      const cardElem = document.createElement('div');
+      cardElem.classList.add('menu__item'); // const cardElem = document.createElement('div');
+
+      cardElem.innerHTML = `<img>
+                <h3 class="menu__item-subtitle"></h3>
+                <div class="menu__item-descr"></div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span></span> грн/день</div>
+                </div>`;
+      this.cardElem = cardElem;
+    } // Вставляет карточку меню в указанный элемент
+
+
+    insertCard(elem) {
+      if (this.cardElem) {
+        elem.append(this.cardElem);
+      } else {
+        throw new Error('Карточки меню не существует');
+      }
+    } // Наполняет карточку данными
+
+
+    fillCard() {
+      if (this.cardElem) {
+        console.dir(this.cardElem);
+        document.body.append(this.cardElem);
+        const imgElem = this.cardElem.querySelector('img');
+        console.log(imgElem);
+        const titleElem = this.cardElem.querySelector('.menu__item-subtitle');
+        const descrElem = this.cardElem.querySelector('.menu__item-descr');
+        const priceNumberElem = this.cardElem.querySelector('.menu__item-total > span');
+        imgElem.setAttribute('src', this.imgSrc);
+        imgElem.setAttribute('alt', this.imgAlt);
+        titleElem.textContent = this.title;
+        descrElem.textContent = this.description;
+        priceNumberElem.textContent = this.price;
+      } else {
+        throw new Error('Карточки меню не существует');
+      }
+    }
+
+  } // База данных карточек меню
+
+
+  const menuCardsData = [{
+    imgSrc: 'img/tabs/vegy.jpg',
+    imgAlt: 'vegy',
+    title: 'Меню "Фитнес"',
+    description: `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.
+                Продукт активных и здоровых людей.
+                Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    price: '9'
+  }, {
+    imgSrc: 'img/tabs/elite.jpg',
+    imgAlt: 'elite',
+    title: 'Меню “Премиум”',
+    description: `В меню “Премиум” мы используем не только красивый дизайн упаковки,
+                но и качественное исполнение блюд.
+                Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+    price: '20'
+  }, {
+    imgSrc: 'img/tabs/post.jpg',
+    imgAlt: 'post',
+    title: 'Меню "Постное"',
+    description: `Меню “Постное” - это тщательный подбор ингредиентов:
+                полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки,
+                правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
+    price: '16'
+  }]; // Контейнер карточек меню
+
+  const menuFieldContainerElem = document.querySelector('.menu__field > .container');
+  const menuCardsFragment = document.createDocumentFragment();
+  menuCardsData.forEach(menuCardData => {
+    const imgSrc = menuCardData.imgSrc;
+    const imgAlt = menuCardData.imgAlt;
+    const title = menuCardData.title;
+    const description = menuCardData.description;
+    const price = menuCardData.price;
+    const menuCard = new MenuCard(imgSrc, imgAlt, title, description, price);
+    menuCard.createCardElem();
+    menuCard.fillCard();
+    menuCard.insertCard(menuCardsFragment);
+  });
+  menuFieldContainerElem.append(menuCardsFragment);
 });
 
 /***/ })
